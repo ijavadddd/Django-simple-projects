313d59f24872fb98a691deb47dfc3aff12cfe5be
ijavadddd <ijavaddddd@gmail.com>
2021-12-27 15:04:47 +0330

    Edit home-page template

--- 1-project1/apps/home/views.py
+++ 1-project1/apps/home/views.py
@@ -1,5 +1,14 @@
 from django.shortcuts import render
 from django.http import HttpResponse
-
+from django.conf import settings
 def index(request):
-    return render(request,'base-index.html')
+    listPerson=[    
+                {'profile':'images/javad-ghasemi.jpg','firstName':'جواد','lastName':'قاسمی','age':'20','skills':['Python','Django','SQL Server' ,'HTML', 'CSS', 'JavaScript']},
+                {'profile':'images/mahdi-abbasi.jpg','firstName':'مهدی','lastName':'عباسی','age':'46','skills':['Python','Django','SQL Server' ,'HTML', 'CSS', 'JavaScript','C#','C++','.NET','Java']}
+                ]
+    context={
+        'media_url':settings.MEDIA_URL,
+        'teacher':listPerson,
+        'lenList':range(0 , len(listPerson)),
+    }
+    return render(request,'home/index.html',context)
--- 1-project1/ijavadddd/urls.py
+++ 1-project1/ijavadddd/urls.py
@@ -1,9 +1,10 @@
 from django.contrib import admin
 from django.urls import path,include
-
+from django.conf import settings
+from django.conf.urls.static import static
 urlpatterns = [
     path('admin/', admin.site.urls),
     path('',include('apps.home.urls')),
     path('blog',include('apps.blog.urls')),
     path('login',include('apps.login.urls')),
-]
+]+static(settings.MEDIA_URL,document_root=settings.MEDIA_ROOT)
