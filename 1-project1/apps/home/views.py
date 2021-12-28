8597b5e1da4619aa27ed794d84828908c7fa947a
ijavadddd <ijavaddddd@gmail.com>
2021-12-29 01:03:41 +0330

    set context for send values for boxes

--- 1-project1/apps/home/views.py
+++ 1-project1/apps/home/views.py
@@ -3,8 +3,10 @@ from django.http import HttpResponse
 from django.conf import settings
 def index(request):
     listPerson=[    
-                {'profile':'images/javad-ghasemi.jpg','firstName':'جواد','lastName':'قاسمی','age':'20','skills':['Python','Django','SQL Server' ,'HTML', 'CSS', 'JavaScript']},
-                {'profile':'images/mahdi-abbasi.jpg','firstName':'مهدی','lastName':'عباسی','age':'46','skills':['Python','Django','SQL Server' ,'HTML', 'CSS', 'JavaScript','C#','C++','.NET','Java']}
+                {'profile':'images/javad-ghasemi.jpg','firstName':'جواد','lastName':'قاسمی','age':20,'skills':['Python','Django','SQL Server' ,'HTML', 'CSS', 'JavaScript','Git']},
+                {'profile':'images/mahdi-abbasi.jpg','firstName':'مهدی','lastName':'عباسی','age':46,'skills':['Python','Django','SQL Server' ,'HTML', 'CSS', 'JavaScript','C#','C++','.NET','Java']},
+                {'profile':'images/john.jpg','firstName':'جان','lastName':'ویلسون','age':36,'skills':['Python','Django','SQL Server', 'JavaScript','C#','C++']},
+                {'profile':'images/karim.jpg','firstName':'کریم','lastName':'عبدالله','age':26,'skills':['Git', 'JavaScript','C#','C++']},
                 ]
     context={
         'media_url':settings.MEDIA_URL,
