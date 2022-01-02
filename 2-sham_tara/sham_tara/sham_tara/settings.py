746c7568bc8a7a7daee06bc572b077a639b2d2ab
ijavadddd <ijavaddddd@gmail.com>
2022-01-02 22:21:48 +0330

    Fix media and static Roots

--- 2-sham_tara/sham_tara/sham_tara/settings.py
+++ 2-sham_tara/sham_tara/sham_tara/settings.py
@@ -107,11 +107,11 @@ USE_TZ = True
 
 STATIC_URL = '/static/'
 # STATIC_ROOT = os.path.join(STATIC_URL, 'static/')
-STATICFILES_DIRS=(os.path.join(STATIC_URL, 'static/'),)
+STATICFILES_DIRS=(os.path.join(BASE_DIR, 'static'),)
 
 # Static files (Images)
 MEDIA_URL = '/media/'
-MEDIA_ROOT = os.path.join(STATIC_URL, 'media/')
+MEDIA_ROOT = os.path.join(BASE_DIR, 'media/')
 
 # Default primary key field type
 # https://docs.djangoproject.com/en/4.0/ref/settings/#default-auto-field
