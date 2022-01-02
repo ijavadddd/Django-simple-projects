c34f31f7038149295625c6f28cfd9ab762717865
ijavadddd <ijavaddddd@gmail.com>
2022-01-02 22:44:43 +0330

    set home page in home app views

--- 2-sham_tara/sham_tara/apps/home/views.py
+++ 2-sham_tara/sham_tara/apps/home/views.py
@@ -1,5 +1,9 @@
 from django.shortcuts import render
+from django.conf import settings
 
 # Create your views here.
 def index(request):
-    return render(request, 'base-index.html')
+    context={
+        'media':settings.MEDIA_URL,
+    }
+    return render(request, 'home/home.html',context)
