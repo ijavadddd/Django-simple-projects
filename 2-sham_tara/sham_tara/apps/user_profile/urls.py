de29b3b8df9f8f06e3a49e968becbfb10ef3e924
ijavadddd <ijavaddddd@gmail.com>
2022-01-05 13:44:46 +0330

    Add credit template

--- 2-sham_tara/sham_tara/apps/user_profile/urls.py
+++ 2-sham_tara/sham_tara/apps/user_profile/urls.py
@@ -5,6 +5,7 @@ urlpatterns = [
     path('',views.user_profile,name='user_profile'),
     path('/car_rescuer',views.car_rescuer,name="car_rescuer"),
     path('/discount',views.discount,name="discount"),
+    path('/credit',views.credit,name="credit"),
     
 ]
 
--- 2-sham_tara/sham_tara/apps/user_profile/views.py
+++ 2-sham_tara/sham_tara/apps/user_profile/views.py
@@ -1,20 +1,17 @@
 from django.shortcuts import render
 from django.conf import settings
 # Create your views here.
-def user_profile(request):
-    context = {
-        'media' :settings.MEDIA_URL,
+context={
+        'media':settings.MEDIA_URL,
     }
+def user_profile(request):
     return render(request,'user_profile/user_profile.html',context)
 
 def car_rescuer(request):
-    context={
-        'media':settings.MEDIA_URL,
-    }
     return render(request,'user_profile/car_rescuer.html',context)
 
 def discount(request):
-    context={
-        'media':settings.MEDIA_URL,
-    }
     return render(request,'user_profile/discount.html',context)
+
+def credit(request):
+    return render(request,'user_profile/credit.html',context)
