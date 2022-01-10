0e8f25aa92fa14613b0424156ed38673ba5ed7d2
ijavadddd <ijavaddddd@gmail.com>
2022-01-10 16:00:32 +0330

    Create login-register section

--- 3-jewelry_gallery/jewelry_gallery/static/js/index.js
+++ 3-jewelry_gallery/jewelry_gallery/static/js/index.js
@@ -7,3 +7,21 @@ function responsive_menu() {
         x.className = "responsive-menu-list";
     }
 }
+function open_login_form() {
+    var x = document.getElementById("login-register-container");
+    if (x.className === "login-register-container") {
+        x.className += " show-form";
+    } 
+    else {
+        x.className = "login-register-container";
+    }
+}
+function close_login_form() {
+    var x = document.getElementById("login-register-container");
+    if (x.className === "login-register-container") {
+        x.className += "show-form";
+    } 
+    else {
+        x.className = "login-register-container";
+    }
+}
