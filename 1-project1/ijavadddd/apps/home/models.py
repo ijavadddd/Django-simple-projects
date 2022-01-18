071f79ea23afa23dcdff062988aab7dd0698bed8
ijavadddd <ijavaddddd@gmail.com>
2022-01-18 19:39:44 +0330

    Add Professor model to home app

--- 1-project1/ijavadddd/apps/home/models.py
+++ 1-project1/ijavadddd/apps/home/models.py
@@ -1,3 +1,48 @@
 from django.db import models
 
 # Create your models here.
+class Professor(models.Model):
+    ProfessorId = models.AutoField( 
+                                    unique=True,
+                                    auto_created=True,
+                                    primary_key=True,
+                                    db_column="آیدی",
+                                    verbose_name="ایدی اساتید",
+                                    )
+    FirstName = models.CharField(
+                                    max_length=30,
+                                    null=False,
+                                    db_column ="نام",
+                                    verbose_name="نام",
+                                    )
+    LastName = models.CharField(    
+                                    max_length=30,
+                                    null=False,
+                                    db_column ="نام خانوادگی",
+                                    verbose_name="نام خانوادگی",
+                                    )
+    # Slug = models.SlugField(max_length=60)
+    Skill = models.CharField(
+                                    max_length=120,
+                                    help_text="Skill seprate by , ",
+                                    db_column ="مهارت ها",
+                                    verbose_name="مهارت ها",
+                                    )
+    Age = models.IntegerField(
+                                    db_column="سن",
+                                    )
+    ProfilePhoto = models.CharField(
+                                    default='No7ne',
+                                    max_length=350,
+                                    db_column ="عکس پروفایل",
+                                    verbose_name="عکس پروفایل",
+                                    )
+    Status = models.BooleanField(
+                                    default=True,
+                                    null=False,
+                                    db_column ="وضعیت",
+                                    verbose_name="وضعیت",
+                                    )
+    def __str__(self):
+        Field = f'First name : {self.FirstName} , Last name : {self.LastName} , Status : {self.Status}'
+        return Field
--- 1-project1/ijavadddd/apps/home/views.py
+++ 1-project1/ijavadddd/apps/home/views.py
@@ -1,16 +1,16 @@
 from django.shortcuts import render
 from django.http import HttpResponse
 from django.conf import settings
+from .models import Professor
 def index(request):
-    listPerson=[    
-                {'profile':'images/javad-ghasemi.jpg','firstName':'جواد','lastName':'قاسمی','age':20,'skills':['Python','Django','SQL Server' ,'HTML', 'CSS', 'JavaScript','Git']},
-                {'profile':'images/mahdi-abbasi.jpg','firstName':'مهدی','lastName':'عباسی','age':46,'skills':['Python','Django','SQL Server' ,'HTML', 'CSS', 'JavaScript','C#','C++','.NET','Java']},
-                {'profile':'images/john.jpg','firstName':'جان','lastName':'ویلسون','age':36,'skills':['Python','Django','SQL Server', 'JavaScript','C#','C++']},
-                {'profile':'images/karim.jpg','firstName':'کریم','lastName':'عبدالله','age':26,'skills':['Git', 'JavaScript','C#','C++']},
-                ]
+    professors=Professor.objects.all()
+    for professor in professors:
+        for skills in professor.Skill:
+            skillList=skills.split()
+            skills='ddsss'
     context={
         'media_url':settings.MEDIA_URL,
-        'teacher':listPerson,
-        'lenList':range(0 , len(listPerson)),
+        'teacher':professors,
+        'lenList':range(0 , len(professors)),
     }
     return render(request,'home/index.html',context)
