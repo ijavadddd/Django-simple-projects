bc54c3c9e5cda161ec494d02ec8292561ee12725
ijavadddd <ijavaddddd@gmail.com>
2022-01-19 19:46:04 +0330

    Add blog app models

--- 1-project1/ijavadddd/apps/blog/admin.py
+++ 1-project1/ijavadddd/apps/blog/admin.py
@@ -1,3 +1,15 @@
+from re import A
 from django.contrib import admin
-
+from .models import Author,Article
 # Register your models here.
+@admin.register(Author)
+class AuthorAdmin(admin.ModelAdmin):
+    list_display = ('FirstName','LastName','Education','Email','Status')
+    search_fields = ('Status',)
+    prepopulated_fields ={'Slug':('FirstName','LastName')}
+
+@admin.register(Article)
+class ArticleAdmin(admin.ModelAdmin):
+    list_display = ('Title','Tags','Status')
+    search_fields = ('Status','Tags')
+    prepopulated_fields = {'Slug':('Title',)}
