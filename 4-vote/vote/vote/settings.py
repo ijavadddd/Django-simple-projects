7f6bd7308a4e3e57cfff3f904318c27cf32e25c6
ijavadddd <ijavaddddd@gmail.com>
2022-02-01 18:41:21 +0330

    Complete account template

--- 4-vote/vote/vote/settings.py
+++ 4-vote/vote/vote/settings.py
@@ -119,13 +119,13 @@ USE_TZ = True
 # https://docs.djangoproject.com/en/4.0/howto/static-files/
 
 STATIC_URL = '/static/'
-STATICFILES_DIRS = ((os.path.join(STATIC_URL, 'static/')),)
+STATICFILES_DIRS=((os.path.join(BASE_DIR, 'static/')),)
 
 
 # Media files (Images, Sound, Videos)
 
 MEDIA_URL = '/media/'
-MEDIA_ROOT = os.path.join(MEDIA_URL, 'media/')
+MEDIA_ROOT=os.path.join(BASE_DIR, 'media/')
 
 # Default primary key field type
 # https://docs.djangoproject.com/en/4.0/ref/settings/#default-auto-field
